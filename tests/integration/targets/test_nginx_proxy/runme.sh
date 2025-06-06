d38324958e8326c13c91733134f46d575cf21f04
Peter Grlica <peter.grlica@gmail.com>
2025-06-06 15:06:07 +0200

    Moved gcp integration tests out of aws ones

--- tests/integration/targets/test_nginx_proxy/runme.sh
+++ tests/integration/targets/test_nginx_proxy/runme.sh
@@ -3,17 +3,9 @@
 set -eu
 
 function cleanup {
-    e=$?
-
     ansible-playbook infra.ai.aws_teardown -e @defaults/main.yml
     unset ANSIBLE_CACHE_PLUGIN
     unset ANSIBLE_CACHE_PLUGIN_CONNECTION
-
-    if [[ -n "${GITHUB_ACTIONS+x}" ]]; then
-        exit $e
-    fi
-
-    ansible-playbook infra.ai.gcp_teardown -e @defaults/main.yml
 }
 
 trap 'cleanup "$@"' EXIT
@@ -26,16 +18,3 @@ ansible-playbook infra.ai.aws_provision -i rhelai.aws_ec2.yml -e @defaults/main.
 
 # test infrastructure post-orchestration
 ansible-playbook test_infra.yml -i rhelai.aws_ec2.yml -e @defaults/main.yml -e action=post
-
-if [[ -n "${GITHUB_ACTIONS+x}" ]]; then
-    exit 0
-fi
-
-# test infrastructure pre-orchestration
-ansible-playbook test_gcp_infra.yml -e @defaults/main.yml -e action=pre -e rhelai_gcp_service_account_file=service-account.json
-
-# provision the Google Cloud infrastructure
-ansible-playbook infra.ai.gcp_provision -e @defaults/main.yml
-
-# test infrastructure post-orchestration
-ansible-playbook test_gcp_infra.yml -e @defaults/main.yml -e action=post -e rhelai_gcp_service_account_file=service-account.json
